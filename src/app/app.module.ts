f16587ce7d5049c55090ac04da0945d5cc51f517
bm280516 <maryvon.michel@gmail.com>
2019-07-18 02:43:40 +0200

    install dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderContainerComponent } from './core/header-container/header-container.component';
+import { BodyContainerComponent } from './core/body-container/body-container.component';
+import { FooterContainerComponent } from './core/footer-container/footer-container.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderContainerComponent,
+    BodyContainerComponent,
+    FooterContainerComponent,
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/core/body-container/body-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-container',
+  templateUrl: './body-container.component.html',
+  styleUrls: ['./body-container.component.scss']
+})
+export class BodyContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
